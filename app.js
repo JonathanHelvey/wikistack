e81eacb54f4aa05e01da3fcf00f7603980e5968c
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-03-07 11:08:36 -0600

    jons

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
-
 const app = express();
+
 //static folder
 app.use(express.static(__dirname + "/public"));
 // parses url-encoded bodies
 app.use(express.urlencoded({ extended: false }));
-
 app.use(morgan("dev"));
+
+app.use('', (req, res) => {
+    res.send('');
+});
+
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+});
