d353c157cf8ebf6b4f5a3cd6464b0a8262f53759
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-03-07 15:59:56 -0600

    3rd commit

--- routs/wiki.js
+++ routs/wiki.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const addPage = require('../views/addPage')
+const { Page } = require("../models");
+
+function generateSlug(title) {
+    return title.replace(/\s+/g, "_").replace(/\W/g, "");
+}
 
 module.exports = router;
 
@@ -12,8 +17,22 @@ router.get('/', (req, res, next) => {
     res.send('got to GET /wiki/');
 });
 
-router.post('/', (req, res, next) => {
-    res.send('got to POST /wiki/')
+router.post('/', async(req, res, next) => {
+    // STUDENT ASSIGNMENT:
+    // add definitions for `title` and `content`
+
+    const page = new Page({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    page.beforeCreate((title) => {
+        page.slug = generateSlug(title);
+    });
+    // make sure we only redirect *after* our save is complete!
+    // note: `.save` returns a promise.
+    try {
+        await page.save();
+        res.redirect('/');
+    } catch (error) { next(error) }
 });
-
-
